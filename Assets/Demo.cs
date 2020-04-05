1f25981f24e48fac3f276efbf389b084540879c0
passion <yupu@synative.com>
2020-04-05 11:32:21 +0800

    fist commit

---
+++ Assets/Demo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Demo : MonoBehaviour
+{
+    void Start()
+    {
+        //初始化GMCosole
+        GMConsole.Instance.Startup();
+        //屏幕最上方，连点4次，打开GM，也可以在GMCosole的UpdateKeyCode自己重新定义
+
+        //自定义GM命令：例如
+        //1.清除缓存命令
+        GMConsole.Instance.RegisterCommandCallback("clear cache",
+        (args) =>
+        {
+            PlayerPrefs.DeleteAll();
+            return "clear cache";
+        }, "清除缓存");
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+# GM Cosole
+
+# 1.初始化
+```
+GMCosole.Instance.StartUp();
+```
+
+# 2.打开
+```
+屏幕最上方，快速连击4次，可以打开和关闭
+```
+
+# 3.注册新的GM命令
+```
+GMConsole.Instance.RegisterCommandCallback("clear cache",(args) =>{
+	PlayerPrefs.DeleteAll();
+	return "clear cache";
+}, "清除缓存");
+            
+```
+
+# 4.关闭GM
+```
+close命令或者屏幕上方再连点4次
+```
+
+# 5.效果图
